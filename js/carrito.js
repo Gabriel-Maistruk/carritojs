8d29260651ddb23b5ba66402ad995cd174e2f1f7
Gabriel Maistruk <gabrielmaistruk99@outlook.com>
2023-09-07 13:29:12 -0300

    carritojs

---
+++ js/carrito.js
@@ -0,0 +1,108 @@
+const pintarCarrito = () =>{
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Productos en carrito:</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement ("h1");
+    modalbutton.innerHTML = `
+    <span class="modal-header-button"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+    </svg> </span>
+    `;
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () =>{
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+        <span class="restar"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+        </svg> </span>
+        <p>cantidad: ${product.cantidad}</p>
+        <span class="sumar"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg> </span>
+        <p>total: ${product.cantidad * product.precio}</p>
+        <span class="delete-product"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+        </svg> </span>
+    `;
+
+    modalContainer.append(carritoContent)
+
+    let restar = carritoContent.querySelector(".restar");
+
+    restar.addEventListener("click", () => {
+        if(product.cantidad !== 1){
+            product.cantidad--;
+        }
+        saveLocal();
+        pintarCarrito ();
+    });
+
+    let sumar = carritoContent.querySelector(".sumar");
+    sumar.addEventListener("click", () => {
+        product.cantidad++;
+        saveLocal();
+        pintarCarrito ();
+    });
+
+    let eliminar = carritoContent.querySelector (".delete-product");
+
+    eliminar.addEventListener("click", () => {
+        eliminarProducto(product.id);
+    });
+
+    });
+
+    const total = carrito.reduce ((acc, el) => acc + el.precio * el.cantidad, 0);
+
+    const totalBuying = document.createElement("div");
+    totalBuying.className =  "total-content";
+    totalBuying.innerHTML =  `total a pagar: ${total} $`;
+    modalContainer.append(totalBuying);
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+
+    carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+    })
+    carritoCounter();
+    saveLocal();
+    pintarCarrito();
+};
+
+const carritoCounter = () =>{
+    cantidadCarrito.style.display = "block";
+
+    const carritoLength = carrito.length;
+
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+carritoCounter();
